fb813778b96cb40b44d8d9fb40a9dd4fbe17d920
ImalshaRC <chaminduimalsha@gmail.com>
2023-01-25 21:20:41 +0530

    third

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-const http = require("http");
+// const http = require("http");
 
-const hostname = "127.0.0.1";
-const port = 3000;
+// const hostname = "127.0.0.1";
+// const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+// const server = http.createServer((req, res) => {
+//   res.statusCode = 200;
+//   res.setHeader("Content-Type", "text/plain");
+//   res.end("Hello World");
+// });
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from node");
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+const port = 5001;
+app.listen(port, () => {
+  console.log(`Server Listening on port ${port}`);
 });
